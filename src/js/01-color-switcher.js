75af0b8ae6febb5a3d97a0125a852c2b4aa9e735
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-02-05 19:21:13 +0200

    hw
    hw

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const dataStartBtn = document.querySelector('[data-start]');
+const dataStopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+dataStartBtn.addEventListener('click', onStart);
+dataStopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(bgColor, 1000);
+
+  dataStartBtn.toggleAttribute('disabled');
+}
+
+
+function onStop() {
+  clearInterval(timerId);
+
+  dataStartBtn.removeAttribute('disabled');
+}
+
+function bgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
